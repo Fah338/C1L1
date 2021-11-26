a9d4d969d082779812093a509d38c444f0154986
Fah338 <Fah_automatic@hotmail.com>
2021-11-26 22:02:35 +0700

    first commit

---
+++ Assets/Scripts/WreckOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WreckOnCollision : MonoBehaviour
+{
+    public GameObject wreckedVersion;//
+
+    // Update is called once per frame
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);//odject ที่ทำงาน
+        Instantiate(wreckedVersion,transform.position,transform.rotation);
+    }//นำตัว2 มาแทนตัวเดิม
+}
